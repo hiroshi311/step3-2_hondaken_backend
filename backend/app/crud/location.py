f28c1a169a94500406fd2d51683430745f7d55b2
ShotaMorimoto-Space <s.morimoto1101@gmail.com>
2025-04-04 23:59:19 +0900

    施設詳細の取得

--- backend/app/crud/location.py
+++ backend/app/crud/location.py
@@ -4,3 +4,6 @@ from app.schemas.location import LocationCreate
 
 def get_locations(db: Session, skip: int = 0, limit: int = 100):
     return db.query(LocationModel).offset(skip).limit(limit).all()
+
+def get_location_by_id(db: Session, location_id: int):
+    return db.query(LocationModel).filter(LocationModel.id == location_id).first()
