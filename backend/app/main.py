990b9cf01ab944417d31fa6b190d491ce191839a
hiroshi311 <fukatsuhiroshi311@gmail.com>
2025-04-05 10:29:14 +0900

    Add user auth feature

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,19 @@ from app.api import dog as dog_api
 from app.api import user as user_api
 from app.api import qrcode as qr_api
 
+from app.core import auth
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # ← フロントのURL
+    allow_credentials=True,
+    allow_methods=["*"],  # ← これがないとPOSTすら通らない！
+    allow_headers=["*"],
+)
+
 
 # テーブル作成（初期構築用）
 Base.metadata.create_all(bind=engine)
@@ -28,3 +40,6 @@ app.include_router(location_api.router)
 app.include_router(reservation_api.router)
 app.include_router(user_api.router)
 app.include_router(qr_api.router)
+
+# clerk用を追加
+app.include_router(auth.router)
